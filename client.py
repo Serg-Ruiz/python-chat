0db80268bf07cf44ab6517ffa1378c39a4baad3d
serg-ruiz <sergioruiz@fullstackprogrammer.net>
2024-09-02 20:37:01 -0500

    added comments to better understand code and when reviewed later

--- client.py
+++ client.py
@@ -2,9 +2,11 @@ import socket
 import sys
 import threading
 
+#function to recieve messages from the server
 def receive_messages(client_socket):
     while True:
         try:
+            #recieve messaage decode it and print it out to the user
             received_msg = client_socket.recv(1024).decode()
             if received_msg:
                 print(f"\n{received_msg}")  # Ensure messages are on a new line
@@ -15,19 +17,21 @@ def receive_messages(client_socket):
         except Exception as e:
             print(f"\nError receiving message: {e}")
             break
+    #when done closes the socket
     client_socket.close()
 
 def main():
+    #tries to create socket if fails error code is thrown ot
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("Socket successfully created")
     except socket.error as err:
         print(f"Socket creation failed with error {err}")
         sys.exit()
-
+    #port that the service will be run on and ip of the server
     port = 1223
     host_ip = '50.76.70.60'
-    
+    #connect to socket on server if it does not suceed throws out error
     try:
         client_socket.connect((host_ip, port))
         print("The socket has successfully connected to the server")
@@ -37,7 +41,7 @@ def main():
 
     # Start a thread to listen for incoming messages
     threading.Thread(target=receive_messages, args=(client_socket,), daemon=True).start()
-
+    #while loop to constantly ask user for input and see if user wants to disconnect
     while True:
         try:
             user_input = input(">>")
--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 import socket
 import threading
 
+#array to store the users 
 users = []
 
+#function to handle what to do once new client connects to server
 def on_new_client(clientsocket, addr):
     print('Got connection from', addr)
-    
+    #while loop to constantly check and recieve if client has sent message
     while True:
         try:
             msg = clientsocket.recv(1024)
-            
+            #log message to the server and then send the message to other clients that are connected
             if msg:
                 print(f'{addr[0]}:{addr[1]} - {msg.decode()}')
                 msg_to_broadcast = f'From {addr[0]}:{addr[1]} - {msg.decode()}'
@@ -21,8 +23,9 @@ def on_new_client(clientsocket, addr):
             print(f'Error receiving message: {e}')
             remove_connection(clientsocket)
             break
-
+#function to handle sending message to other connect connected client
 def broadcast(message: str, sender_socket: socket.socket):
+    #goes through array of users connected and sends it to all users that is not the client that sent that message
     for client_conn in users:
         if client_conn != sender_socket:
             try:
@@ -31,22 +34,27 @@ def broadcast(message: str, sender_socket: socket.socket):
                 print(f'Error broadcasting message: {e}')
                 remove_connection(client_conn)
 
+#function to handle disconencting users
 def remove_connection(conn: socket.socket) -> None:
     if conn in users:
         conn.close()
         users.remove(conn)
 
 def main():
+    #create socket
     s = socket.socket()
     print("Socket successfully created")
-
+    
+    #port that the service will ran on
     port = 1223
     s.bind(('', port))
     print(f"Socket binded to {port}")
-
+    
+    #socket listening for connection
     s.listen(5)
     print("Socket is listening")
-
+    
+    #accept connections and add user to the array of users and create thread to handlen each new user that connects
     while True:
         c, addr = s.accept()
         users.append(c)
